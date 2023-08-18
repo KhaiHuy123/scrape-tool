5bee00d2e63120cb4d2c97a16546a6965eaf8945
nguyenkhaihuy <86825653+KhaiHuy123@users.noreply.github.com>
2023-08-18 22:48:36 +0700

    selenium_project
    automate and web-scraping using Selenium

---
+++ selenium_project/automation_bot_data_scraping/auto_bot/auto_purchase/automatic_bot_purchase_books_Fahasa.py
@@ -0,0 +1,154 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.edge.service import Service
+from selenium.webdriver.support.ui import WebDriverWait
+try:
+    website = 'https://www.fahasa.com/'
+    driver_path = "D:\microsoftdriver_autotest_112\msedgedriver.exe"
+    def select_place_product(product):
+        search_field = driver.find_element(By.XPATH, value='//*[@id="search_desktop"]')
+        search_field.send_keys(product)
+        search_button = driver.find_element(By.XPATH, value='//*[@id="search_mini_form_desktop"]/div/div/span')
+        search_button.click()
+    def change_to_in_stock_area(driver):
+        in_stock = driver.find_element(By.XPATH, value='//*[@class="selectBox selectBox-in-stock"]')
+        in_stock.click()
+        select_in_stock_option = driver.find_element(By.XPATH, value='//*[@class="selectOption selectOption-in-stock"]')
+        select_in_stock_option.click()
+    def choose_product(driver):
+        driver.implicitly_wait(15)
+        product = driver.find_element(By.XPATH,
+        value='//*[@id="wrapper"]/div[2]/div[1]/div/div[2]/div/div/div[2]/div/div[5]/div[2]/ul/li[1]/div/div[2]/h2/a')
+        WebDriverWait(driver, 5)
+        product.click()
+    def add_to_cart(driver):
+        driver.implicitly_wait(15)
+        cart = driver.find_element(By.CSS_SELECTOR, value='.btn-buy-now')
+        cart.click()
+    def payment(driver):
+        driver.implicitly_wait(15)
+        driver.refresh()
+        payment = driver.find_element(By.XPATH, value='//*[@id="checkbox-all-products"]')
+        payment.click()
+    def submit(driver):
+        driver.implicitly_wait(15)
+        driver.refresh()
+
+        ##################################################################################################################################
+
+        pay_button = driver.find_element(By.XPATH, value='//*[@id="form-cart"]/div/div[2]/div/div/div[3]/div[2]/div/button')
+        pay_button.click()
+
+        ##################################################################################################################################
+
+        name = driver.find_element(By.XPATH, value='//*[@id="fhs_shipping_fullname"]')
+        name.send_keys(f'{name_client}')
+
+        ##################################################################################################################################
+
+        email = driver.find_element(By.XPATH, value='//*[@id="fhs_shipping_email"]')
+        email.send_keys(f'{email_recv}')
+
+        ##################################################################################################################################
+
+        phone_number = driver.find_element(By.XPATH, value='//*[@id="fhs_shipping_telephone"]')
+        phone_number.send_keys(f'{phone}')
+
+        ##################################################################################################################################
+
+        city_province = driver.find_element(By.CSS_SELECTOR,
+        value='#select2-fhs_shipping_city_select-container')
+        city_province.click()
+        input_city = driver.find_element(By.XPATH,
+        value='//*[@id="offcanvas-container"]/span/span/span[1]/input')
+        input_city.send_keys(f'{city}')
+        city_choice = driver.find_element(By.XPATH,
+        value='//*[@id="select2-fhs_shipping_city_select-results"]/li[1]')
+        city_choice.click()
+        time.sleep(1)
+
+        ##################################################################################################################################
+
+        district = driver.find_element(By.CSS_SELECTOR,
+        value='#select2-fhs_shipping_district_select-container')
+        district.click()
+        input_district = driver.find_element(By.XPATH,
+        value='//*[@id="offcanvas-container"]/span/span/span[1]/input')
+        input_district.send_keys(f'{district_name}')
+        district_choice = driver.find_element(By.XPATH,
+        value='//*[@id="select2-fhs_shipping_district_select-results"]/li[1]')
+        district_choice.click()
+        time.sleep(5)
+
+        ##################################################################################################################################
+
+        ward = driver.find_element(By.CSS_SELECTOR,
+        value='#select2-fhs_shipping_wards_select-container')
+        ward.click()
+        input_ward = driver.find_element(By.XPATH,
+        value='//*[@id="offcanvas-container"]/span/span/span[1]/input')
+        input_ward.send_keys(f'{ward_name}')
+        ward_choice = driver.find_element(By.XPATH,
+        value='//*[@id="select2-fhs_shipping_wards_select-results"]/li')
+        ward_choice.click()
+        time.sleep(1)
+
+        ##################################################################################################################################
+
+        adress = driver.find_element(by='xpath', value='//*[@id="fhs_shipping_street"]')
+        adress.send_keys(f'{adress_name}')
+    def execute():
+        # send keys to input field (product's name) and click search button
+        select_place_product(name_product)
+        # move to available product list
+        change_to_in_stock_area(driver)
+        # click to product
+        choose_product(driver)
+        # Add to cart
+        add_to_cart(driver)
+        # Start payment process
+        payment(driver)
+        # Submit
+        submit(driver)
+        time.sleep(15)
+        driver.quit()
+    def input_info():
+        global name_product, name_client, email_recv, phone, city, district_name, ward_name, adress_name
+        name_product = input('Name_product : ')
+        name_client = input('Client : ')
+        email_recv = input('Email : ')
+        phone = input('Phone : ')
+        city = input('City : ')
+        district_name = input('District : ')
+        ward_name = (input('Ward : '))
+        adress_name = input('Adress : ')
+    #################################################################################################################################
+
+    input_info()
+
+    #################################################################################################################################
+
+    service = Service(executable_path=driver_path)
+    options = webdriver.EdgeOptions()
+    options.add_argument("--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko")
+    driver = webdriver.Edge(service=service, options=options)
+    driver.implicitly_wait(15)
+    driver.maximize_window()
+    driver.get(website)
+
+    #################################################################################################################################
+
+    execute()
+
+except Exception as e:
+    if 'in PATH' in str(e):
+        print(
+            'You are trying to run the bot from command line \n'
+            'Please add to PATH your Selenium Drivers \n'
+            'Windows: \n'
+            '    set PATH=%PATH%;C:path-to-your-folder \n \n'
+            'Linux: \n'
+            '    PATH=$PATH:/path/toyour/folder/ \n'
+        )
+    else:raise
---
+++ selenium_project/automation_bot_data_scraping/auto_bot/chain_working/web_info_manga_truyenqqi.py
@@ -0,0 +1,99 @@
+import time
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.edge.service import Service
+class web_info_():
+    def __init__(self):
+        path = "D:\microsoftdriver_autotest_114\msedgedriver.exe"
+        service = Service(executable_path=path)
+        options = webdriver.EdgeOptions()
+        options.add_argument("--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36 Edge/90.0.864.75")
+        options.add_argument('--disable-popup-blocking')
+        self.driver = webdriver.Edge(service=service, options=options)
+        self.urls = []
+        # website can be changed, replace with a suitable url
+        self.website = "https://truyenqq.com.vn/"
+
+        self.driver.implicitly_wait(15)
+
+    def get_current_url(self):
+        return self.driver.current_url
+
+    def input_data(self):
+        self.id_manga_type = int(input('Manga id for search : '))
+        self.num_pages = int(input('Num pages scrape : '))
+        self.file_name = input('File name store url : ')
+
+    def search(self, id):
+
+        self.driver.get(self.website)
+        self.driver.maximize_window()
+        time.sleep(2)
+
+        #######################################################################################################################################
+
+        self.action = ActionChains(self.driver)
+        self.driver.implicitly_wait(15)
+        self.find_ = self.driver.find_element(By.XPATH,
+        value='//*[@id="header_left_menu"]/li[6]/a[@class="tags_name pc_hover"]').get_attribute("href")
+        self.driver.get(self.find_)
+        time.sleep(2)
+
+        xpath_expression =f'//*[@id="main_homepage"]/div[2]/div[2]/div[3]/div[2]/div/span[@data-id={id}]'
+        self.choice = self.driver.find_element(By.XPATH, value=xpath_expression)
+        self.action.move_to_element(self.choice)
+        self.action.click()
+        self.action.perform()
+        self.driver.implicitly_wait(15)
+        self.start_finding = self.driver.find_element(By.XPATH,
+        value='//*[@id="main_homepage"]/div[2]/div[2]/div[6]/div/button[@class="btn btn-success btn-search is-danger"]')
+
+        self.start_finding.click()
+        current_page = self.driver.current_url
+        return current_page
+        pass
+
+    def navigate_to_next_page(self):
+        for i in range(200):
+            self.driver.execute_script(f"window.scrollTo(0, {str(i)}00);")
+        next_button = self.driver.find_element(By.XPATH,
+        value='//*[@id="main_homepage"]/div[5]/a[8]/p')
+        self.action.move_to_element(next_button)
+        self.action.click()
+        self.action.perform()
+
+    def get_all_urls(self, page, num_pages, file_name):
+        self.driver.get(page)
+        current_page = self.driver.current_url
+        first_page_done = True
+        for i in range(num_pages):
+            self.urls.append(current_page)
+            if(first_page_done):
+                next_button = self.driver.find_element(By.XPATH,
+                value='//*[@id="main_homepage"]/div[5]/a[6]/p')
+                self.action.move_to_element(next_button)
+                self.action.click()
+                self.action.perform()
+                first_page_done = False
+                current_page = self.driver.current_url
+                time.sleep(2)
+                continue
+            self.navigate_to_next_page()
+            time.sleep(2)
+            current_page = self.driver.current_url
+        with open(f'.//url_list//{file_name}', 'w') as f:
+            for url in self.urls:
+                f.write(url + '\n')
+
+    def execute(self):
+        self.input_data()
+        first_page = self.search(self.id_manga_type)
+        self.get_all_urls(first_page, self.num_pages, file_name=self.file_name)
+        pass
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.driver.quit()
+if __name__ == '__main__':
+    ob = web_info_()
+    ob.execute()
