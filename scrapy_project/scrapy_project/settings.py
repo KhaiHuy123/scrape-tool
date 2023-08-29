7c6e0bbe2e201f3d516d4d4d6cb05a884d03e370
nguyenkhaihuy <86825653+KhaiHuy123@users.noreply.github.com>
2023-08-29 21:48:38 +0700

    update settings.py

--- scrapy_project/scrapy_project/settings.py
+++ scrapy_project/scrapy_project/settings.py
@@ -47,13 +47,10 @@ DOWNLOAD_DELAY = 8
 #}
 
 # ---------------------------Fake user agent & rotate proxy --------------------------- #
-#SCRAPEOPS_API_KEY = '20e9292e-fd6d-4fc1-8b0c-17a99fae2f5c'
-#SCRAPEOPS_API_KEY = 'ce192c1d-8164-4ff8-bec5-406caab947e5'
-SCRAPEOPS_API_KEY = '0a06c182-96ab-4eb8-8d94-3e640f06e7bb'
-#SCRAPEOPS_API_KEY = '3e88701d-4cf5-4aee-bc7d-fcc66edec0b0'
-SCRAPROPS_FAKE_USER_AGENT_ENDPOINT = 'https://headers.scrapeops.io/v1/user-agents'
+SCRAPEOPS_API_KEY = ''
+SCRAPROPS_FAKE_USER_AGENT_ENDPOINT = ''
 SCRAPEOPS_FAKE_USER_AGENT_ENABLED = True
-SCRAPROPS_FAKE_BROWSER_HEADER_ENDPOINT = 'https://headers.scrapeops.io/v1/browser-headers'
+SCRAPROPS_FAKE_BROWSER_HEADER_ENDPOINT = ''
 SCRAPEOPS_FAKE_BROWSER_HEADER_ENABLED = True
 SCRAPEOPS_NUM_RESULTS = 50
 
@@ -61,15 +58,6 @@ SCRAPEOPS_NUM_RESULTS = 50
 #SCRAPERAPI_CURL_X_COMMAND : "http://scraperapi:4505a4f35383711d43debabd75d2e148@proxy-server.scraperapi.com:8001"
 
 SCRAPERAPI_PROXY_PARAMS = [
-    ['scraperapi', 'proxy-server.scraperapi.com:8001', '67ffab08154296f41b13d17021872c7f']
-    # ['scraperapi', 'proxy-server.scraperapi.com:8001', '1e8bad5dca50612f6cbbf5e76cb41358'],
-    # ['scraperapi', 'proxy-server.scraperapi.com:8001', '29b3b8edc46f3b4b6d2e620e01d6c19c'],
-    # ['scraperapi', 'proxy-server.scraperapi.com:8001', '3b0100de75c46a0bbcd541d1b5027847'],
-    # ['scraperapi', 'proxy-server.scraperapi.com:8001', '3c61b1bc72ed3df21add1475f585c2de'],
-    # ['scraperapi', 'proxy-server.scraperapi.com:8001', '9c7fb8da0711e472484b601692891728'],
-    # ['scraperapi', 'proxy-server.scraperapi.com:8001', '6a20c3c503c77c50ecf17782942ad911'],
-    # ['scraperapi', 'proxy-server.scraperapi.com:8001', '7a63e41562f6929273409e761f013342'],
-    # ['scraperapi', 'proxy-server.scraperapi.com:8001', '32c1affc526a22f4266837ef5ed241f0']
     # ['user_name', 'proxy_end_point', 'api_key']
     # Add more
 ]
@@ -125,7 +113,7 @@ ITEM_PIPELINES = {
 }
 
 # --------------------------- Parameters of Saveto_sqlServerPipeline --------------------------- #
-SQL_SERVER = 'DESKTOP-LMGN073\SQLEXPRESS'
+SQL_SERVER = ''
 #SQL_DATABASE = 'manga'
 #SQL_DATABASE = 'manga_nettruyen'
 #SQL_DATABASE = 'manga_truyenqqi'
@@ -134,7 +122,7 @@ SQL_DATABASE = 'ONLINEBOOKS'
 #SQL_DATABASE = 'product_web_scraping'
 #SQL_PASSWORD = 'password' # No need if using local database engine DBSM
 SQL_AUTHENTICATION = 'Trusted_Connection=yes'
-SQL_USERNAME = 'DESKTOP-LMGN073\HTH'
+SQL_USERNAME = ''
 
 # --------------------------- Stored data in flat file (csv) --------------------------- #
 #FEEDS = {
