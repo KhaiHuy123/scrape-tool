6362c82ca1fbf6c48bd7d11e5bb41b9c5d0747f0
nguyenkhaihuy <86825653+KhaiHuy123@users.noreply.github.com>
2023-08-29 21:45:56 +0700

    update settings.py

--- scrapy_project/scrapy_project/settings.py
+++ scrapy_project/scrapy_project/settings.py
@@ -14,6 +14,7 @@ NEWSPIDER_MODULE = "scrapy_project.spiders"
 # ------------------------- Crawl responsibly by identifying yourself (and your website) on the user-agent --------------------------- #
 #USER_AGENT = "scrapy_project (+http://www.yourdomain.com)"
 #USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36 Edge/90.0.864.75"
+#USER_AGENT ='Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36'
 
 # ------------------------- Obey robots.txt rules --------------------------- #
 #ROBOTSTXT_OBEY = False
@@ -35,6 +36,9 @@ DOWNLOAD_DELAY = 8
 
 # --------------------------- Disable Telnet Console (enabled by default) --------------------------- #
 #TELNETCONSOLE_ENABLED = False
+#TELNETCONSOLE_ENABLED = True
+#TELNETCONSOLE_PASSWORD = 'your_password_here'
+#TELNETCONSOLE_PORT = [6023,6073]
 
 # --------------------------- Override the default request headers: --------------------------- #
 #DEFAULT_REQUEST_HEADERS = {
@@ -43,11 +47,13 @@ DOWNLOAD_DELAY = 8
 #}
 
 # ---------------------------Fake user agent & rotate proxy --------------------------- #
-
-SCRAPEOPS_API_KEY = 'YOUR API KEY'
-SCRAPROPS_FAKE_USER_AGENT_ENDPOINT = 'USER AGENT ENDPOINT'
+#SCRAPEOPS_API_KEY = '20e9292e-fd6d-4fc1-8b0c-17a99fae2f5c'
+#SCRAPEOPS_API_KEY = 'ce192c1d-8164-4ff8-bec5-406caab947e5'
+SCRAPEOPS_API_KEY = '0a06c182-96ab-4eb8-8d94-3e640f06e7bb'
+#SCRAPEOPS_API_KEY = '3e88701d-4cf5-4aee-bc7d-fcc66edec0b0'
+SCRAPROPS_FAKE_USER_AGENT_ENDPOINT = 'https://headers.scrapeops.io/v1/user-agents'
 SCRAPEOPS_FAKE_USER_AGENT_ENABLED = True
-SCRAPROPS_FAKE_BROWSER_HEADER_ENDPOINT = 'BROWSER HEADER ENDPOINT'
+SCRAPROPS_FAKE_BROWSER_HEADER_ENDPOINT = 'https://headers.scrapeops.io/v1/browser-headers'
 SCRAPEOPS_FAKE_BROWSER_HEADER_ENABLED = True
 SCRAPEOPS_NUM_RESULTS = 50
 
@@ -55,6 +61,15 @@ SCRAPEOPS_NUM_RESULTS = 50
 #SCRAPERAPI_CURL_X_COMMAND : "http://scraperapi:4505a4f35383711d43debabd75d2e148@proxy-server.scraperapi.com:8001"
 
 SCRAPERAPI_PROXY_PARAMS = [
+    ['scraperapi', 'proxy-server.scraperapi.com:8001', '67ffab08154296f41b13d17021872c7f']
+    # ['scraperapi', 'proxy-server.scraperapi.com:8001', '1e8bad5dca50612f6cbbf5e76cb41358'],
+    # ['scraperapi', 'proxy-server.scraperapi.com:8001', '29b3b8edc46f3b4b6d2e620e01d6c19c'],
+    # ['scraperapi', 'proxy-server.scraperapi.com:8001', '3b0100de75c46a0bbcd541d1b5027847'],
+    # ['scraperapi', 'proxy-server.scraperapi.com:8001', '3c61b1bc72ed3df21add1475f585c2de'],
+    # ['scraperapi', 'proxy-server.scraperapi.com:8001', '9c7fb8da0711e472484b601692891728'],
+    # ['scraperapi', 'proxy-server.scraperapi.com:8001', '6a20c3c503c77c50ecf17782942ad911'],
+    # ['scraperapi', 'proxy-server.scraperapi.com:8001', '7a63e41562f6929273409e761f013342'],
+    # ['scraperapi', 'proxy-server.scraperapi.com:8001', '32c1affc526a22f4266837ef5ed241f0']
     # ['user_name', 'proxy_end_point', 'api_key']
     # Add more
 ]
@@ -64,6 +79,7 @@ PROXY_USER = ''
 PROXY_PASSWORD = ''
 PROXY_ENDPOINT = ''
 PROXY_PORT = ''
+
 # --------------------------- Time out for response --------------------------- #
 #DOWNLOAD_TIMEOUT = 180
 
@@ -78,8 +94,8 @@ SPIDER_MIDDLEWARES = {
 DOWNLOADER_MIDDLEWARES = {
    "scrapy_project.middlewares.ScrapyProjectDownloaderMiddleware": 543,
    #"scrapy_project.middlewares.ScrapeOpsFakeUserAgentMiddleware":400,
-   "scrapy_project.middlewares.ScrapeOpsFakeBrowserHeaderAgentMiddleware":400,
-   "scrapy_project.middlewares.RotateProxyMiddleware":600,
+   "scrapy_project.middlewares.ScrapeOpsFakeBrowserHeaderAgentMiddleware": 400,
+   "scrapy_project.middlewares.RotateProxyMiddleware": 600,
 }
 
 # --------------------------- Enable or disable extensions --------------------------- #
@@ -91,10 +107,10 @@ DOWNLOADER_MIDDLEWARES = {
 # --------------------------- Configure item pipelines --------------------------- #
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 ITEM_PIPELINES = {
-   #"scrapy_project.pipelines.ScrapyProjectPipeline" : 300,
+   "scrapy_project.pipelines.ScrapyProjectPipeline" : 300,
    #"scrapy_project.pipelines.AmazonPipeline" : 300,
-   "scrapy_project.pipelines.FahasaPipeline" : 300,
-   #"scrapy_project.pipelines.FahasaListPipeline" : 300,
+   #"scrapy_project.pipelines.FahasaPipeline" : 300,
+   "scrapy_project.pipelines.FahasaListPipeline" : 300,
    #"scrapy_project.pipelines.NettruyenPipeLine" : 300,
    #"scrapy_project.pipelines.TruyenqqiPipeline" : 300,
    #"scrapy_project.pipelines.TheSunPipeline" : 300,
@@ -103,21 +119,22 @@ ITEM_PIPELINES = {
    #"scrapy_project.pipelines.Saveto_sqlServerTruyenqqiPipeline" : 400,
    #"scrapy_project.pipelines.Saveto_sqlServerAmazonPipeline" : 400,
    #"scrapy_project.pipelines.Saveto_sqlServerFahasaPipeline" : 400,
-   #"scrapy_project.pipelines.Saveto_sqlServerFahasaPipeline_finalstate": 550,
-   #"scrapy_project.pipelines.Saveto_sqlServerFahasaListPipeline" : 400,
+   #"scrapy_project.pipelines.Saveto_sqlServerFahasaPipeline_finalstate" : 550,
+   "scrapy_project.pipelines.Saveto_sqlServerFahasaListPipeline" : 400,
    "scrapy_project.pipelines.FileWriterPipeline" : 350
 }
 
 # --------------------------- Parameters of Saveto_sqlServerPipeline --------------------------- #
-SQL_SERVER = 'YOUR SERVER'
+SQL_SERVER = 'DESKTOP-LMGN073\SQLEXPRESS'
 #SQL_DATABASE = 'manga'
 #SQL_DATABASE = 'manga_nettruyen'
 #SQL_DATABASE = 'manga_truyenqqi'
-SQL_DATABASE = 'online_books'
+SQL_DATABASE = 'ONLINEBOOKS'
+#SQL_DATABASE = 'DB_bookstore'
 #SQL_DATABASE = 'product_web_scraping'
 #SQL_PASSWORD = 'password' # No need if using local database engine DBSM
 SQL_AUTHENTICATION = 'Trusted_Connection=yes'
-SQL_USERNAME = 'USER NAME'
+SQL_USERNAME = 'DESKTOP-LMGN073\HTH'
 
 # --------------------------- Stored data in flat file (csv) --------------------------- #
 #FEEDS = {
@@ -127,10 +144,12 @@ SQL_USERNAME = 'USER NAME'
 # --------------------------- RETRY --------------------------- #
 RETRY_HTTP_CODES = [499]  # Retry for "499" status code
 handle_httpstatus_list = [500]
+
 # --------------------------- REDIRECT --------------------------- #
 REDIRECT_ENABLED = True
 HTTPERROR_ALLOWED_CODES = [301]
 HTTPERROR_ALLOW_ALL = True
+
 # --------------------------- Enable and configure the AutoThrottle extension (disabled by default) --------------------------- #
 # See https://docs.scrapy.org/en/latest/topics/autothrottle.html
 #AUTOTHROTTLE_ENABLED = True
